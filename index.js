87515e4130c3e215024460deb76da8e053659625
robson-muniz <robson_muniz@yahoo.com>
2019-09-17 15:55:05 -0300

    API Calls with Parameters.

--- index.js
+++ index.js
@@ -17,20 +17,31 @@ app.post("/", function(req, res){
 
   var crypto = req.body.crypto;
   var fiat = req.body.fiat;
+  var amount = req.body.amount;
 
-  var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/"
-  var finalURL = baseURL + crypto + fiat;
+  
+  var options = {
+    url:"https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount,
+    }
+  }
 
-  request(finalURL, function(error, response, body){
+  request(options, function(error, response, body){
 
     var data = JSON.parse(body);
-    var price = data.last;
+    var price = data.price;
 
-    var currentDate = data.display_timestamp;
+    console.log(price);
+
+    var currentDate = data.time;
 
     res.write("<p>The current date is " + currentDate + "</p>");
 
-    res.write("<h1>The current price of " + crypto + " is " + price + fiat + " </h1>");
+    res.write("<h1>" + amount + crypto + " is currently worth" + price + fiat + " </h1>");
 
     res.send();
 
